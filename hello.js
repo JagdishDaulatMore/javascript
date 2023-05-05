e0228f904fe6b69274a8c874f734ae7d1e0b8b85
Jagdish More <jagdishmore51@gmail.com>
2023-05-05 10:54:11 +0530

    Develop two files hello.js and 01_variable_assign.js
    Develop two files hello.js and 01_variable_assign.js

---
+++ hello.js
@@ -0,0 +1,23 @@
+
+
+
+console.log(" Jagdish More. Address: Hinjewadi, Pune");
+
+
+var firstName = "Jagdish";
+console.log(firstName);
+
+var age;        // Variable Declaration
+age = "22";    // Variable Initialization
+console.log(age); //22
+
+var holder;  // Variable Declaration
+console.log(holder);  //undefined
+holder = "My Spec";  // Variable Initialization
+console.log(holder); //My Spec
+
+holder = "Sachin Spec"; // Variable Update
+console.log(holder);    //Sachin Spec
+
+
+
