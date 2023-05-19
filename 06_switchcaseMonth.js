b691bb275cda4fef2e1712729bf58e35c77839ea
Jagdish More <jagdishmore51@gmail.com>
2023-05-19 22:11:54 +0530

    Develop 2 files for Switch Case
    Develop 2 files for Switch Case

---
+++ 06_switchcaseMonth.js
@@ -0,0 +1,63 @@
+
+function monthsInYear(monthNum) {
+    switch (monthNum) {
+      case 1:
+        console.log(`Month is: January as Month number is ${monthNum}`);
+        break;
+      case 2:
+        console.log(`Month is: February as Month number is ${monthNum}`);
+        break;
+      case 3:
+        console.log(`Month is: March as Month number is ${monthNum}`);
+        break;
+      case 4:
+        console.log(`Month is: April as Month number is ${monthNum}`);
+        break;
+      case 5:
+        console.log(`Month is: May as Month number is ${monthNum}`);
+        break;
+      case 6:
+        console.log(`Month is: June as Month number is ${monthNum}`);
+        break;
+      case 7:
+        console.log(`Month is: July as Month number is ${monthNum}`);
+        break;
+        case 8:
+        console.log(`Month is: August as Month number is ${monthNum}`);
+        break;
+        case 9:
+        console.log(`Month is: September as Month number is ${monthNum}`);
+        break;
+        case 10:
+        console.log(`Month is: October as Month number is ${monthNum}`);
+        break;
+        case 11:
+        console.log(`Month is: November as Month number is ${monthNum}`);
+        break;
+        case 12:
+        console.log(`Month is: December as Month number is ${monthNum}`);
+        break;
+      default:
+        console.log(`Invalid input ==>  ${monthNum}`);
+        break;
+    }
+  }
+  monthsInYear(1);
+  monthsInYear(2);
+  monthsInYear(3);
+  monthsInYear(4);
+  monthsInYear(5);
+  monthsInYear(6);
+  monthsInYear(7);
+  monthsInYear(8);
+  monthsInYear(9);
+  monthsInYear(10);
+  monthsInYear(11);
+  monthsInYear(12);
+  
+
+  monthsInYear(null);
+  monthsInYear(undefined);
+  monthsInYear(NaN);
+  
+  
