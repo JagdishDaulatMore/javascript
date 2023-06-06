e6fe4b86d206a2d5706574e1944ac70ca784a018
Jagdish More <jagdishmore51@gmail.com>
2023-06-06 13:50:35 +0530

    Develop a file for word characters to Uppercase
    Develop a file for word characters to Uppercase

---
+++ 16_stringWordUppercase.js
@@ -0,0 +1,21 @@
+ console.log(`=======Each words first and last character is Uppercase=======`);
+ 
+ 
+const str = "How are you mate";
+console.log(`Given string is: ${str}`);
+let newChar = "";
+const arrayWords = str.split(" ");
+for (const element of arrayWords){
+for (let index= 0; index < element.length; index++){
+    let char ="";
+    if(index==0|| index== element.length-1){
+        char = element[index].toUpperCase();
+    }else{
+        char = element[index];
+    }
+    newChar = newChar + char;
+}
+newChar = newChar + " ";
+
+}
+console.log(`${newChar}`); 
